d2316d2deffbbf46cfabdd844b507e17fb77e21b
syed hassaan <hassan.ahmed@eonhealth.com>
2020-11-18 22:14:35 +0500

    implemented 8mp camera

---
+++ SOLID_OOD/Services/Camera.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_OOD.Services
+{
+    public class Camera : ICamera
+    {
+        private readonly int Pixels = 8;
+        public void CapturePicture()
+        {
+            Console.WriteLine($"Captured picture with {Pixels} pixels.");
+        }
+    }
+}
