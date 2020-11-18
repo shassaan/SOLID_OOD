4f03236ed6dd2ee93698afda1c3e139194533993
syed hassaan <hassan.ahmed@eonhealth.com>
2020-11-18 21:46:50 +0500

    created a method to register services

--- SOLID_OOD/ExtensionMethods.cs
+++ SOLID_OOD/ExtensionMethods.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,9 @@ namespace SOLID_OOD
 {
     public static class ExtensionMethods
     {
+        public static void RegisterServices(this ServiceCollection services)
+        {
+            
+        }
     }
 }
