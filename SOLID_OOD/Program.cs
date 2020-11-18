1ecd9905c6bff6dd12db70f91d3cd752fa0f0974
syed hassaan <hassan.ahmed@eonhealth.com>
2020-11-18 21:41:52 +0500

    Add project files.

---
+++ SOLID_OOD/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SOLID_OOD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
