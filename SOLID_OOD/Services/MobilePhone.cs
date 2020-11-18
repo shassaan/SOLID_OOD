32bc198f9d713e86bd9bb5b39364f06b47ac4520
syed hassaan <hassan.ahmed@eonhealth.com>
2020-11-18 22:04:26 +0500

    impleemnted MobilePhone

---
+++ SOLID_OOD/Services/MobilePhone.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_OOD.Services
+{
+    public class MobilePhone : IMobilePhone
+    {
+        private readonly ICallService callService;
+        public MobilePhone(ICallService callService)
+        {
+            this.callService = callService;
+        }
+        public void Call(string contact)
+        {
+            callService.Call(contact);
+        }
+
+        public void RecieveCall(string contact)
+        {
+            callService.RecieveCall(contact);
+        }
+    }
+}
