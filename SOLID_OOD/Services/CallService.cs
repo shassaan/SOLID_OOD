962ef8cb2b02efb5382faceafea97dacc8dd0d3f
syed hassaan <hassan.ahmed@eonhealth.com>
2020-11-18 22:02:26 +0500

    Implemented CallService

---
+++ SOLID_OOD/Services/CallService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_OOD.Services
+{
+    public class CallService : ICallService
+    {
+        public void Call(string contact)
+        {
+            Console.WriteLine($"Calling.. {contact}");
+        }
+
+        public void RecieveCall(string contact)
+        {
+            Console.WriteLine($"Recieved call of {contact}");
+        }
+    }
+}
